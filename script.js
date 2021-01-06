f29aebef5c47cef2aae26edac4020b4adeebd7eb
kmainyv <kmainyv@yandex.ru>
2021-01-06 15:08:54 +0400

    add code
    add automatic generation of the blocks after the page has loaded

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ clearbox.onclick = function () {
     while (boarderBox.firstChild) {
         boarderBox.removeChild(boarderBox.firstChild);
     }
-};
+}
 
 delbox.onclick = function () {
     const boarderBox = document.getElementById('boardbox');
     if (boarderBox.lastElementChild) {
         boarderBox.removeChild(boarderBox.lastElementChild);
     }
-};
+}
 
 insbox.onclick = function () {
     let box = document.createElement('div');
@@ -18,4 +18,13 @@ insbox.onclick = function () {
     box.innerText = boardbox.childElementCount + 1;
     boardbox.appendChild(box);
 
-};
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    for (let i = 1; i <= 8; i++) {
+        let box = document.createElement('div');
+        box.className = 'box';
+        box.innerText = boardbox.childElementCount + 1;
+        boardbox.appendChild(box);
+    }
+}, false);
